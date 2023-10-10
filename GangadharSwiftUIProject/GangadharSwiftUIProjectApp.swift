5a0c8b651c94eae4f99b59942eb2e5a5e43b65b8
Gangadhar C <gangadharganga90@gmail.com>
2023-10-10 12:13:49 +0530

    Initial Commit

---
+++ GangadharSwiftUIProject/GangadharSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GangadharSwiftUIProjectApp.swift
+//  GangadharSwiftUIProject
+//
+//  Created by Gangadhar C on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GangadharSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
