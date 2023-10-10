237c77bc0b5e2d7e7cbaa00acf30757beeded836
Gangadhar C <gangadharganga90@gmail.com>
2023-10-10 16:36:02 +0530

    ADD ApiConfig Filed Added

---
+++ GangadharSwiftUIProject/Common/ApiConfig.swift
@@ -0,0 +1,14 @@
+//
+//  ApiConfig.swift
+//  GangadharSwiftUIProject
+//
+//  Created by Gangadhar C on 10/10/23.
+//
+import Foundation
+ 
+public class WebContants{
+    static let baseURL = "https://reqres.in/api/"
+    static let login = "login"
+    static let users = "users?page="
+    static let user = "users/2"
+}
